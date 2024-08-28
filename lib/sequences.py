f5abc07e1b334c3c065281dc31aae4e76f2961f2
Bethwel123 <bethwellangat654@gmail.com>
2024-08-28 15:30:38 +0300

    Creaded a fibonacci with use of sequence data types

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+        return
+    
+    print_fibonacci = []
+
+    for i in range(length):
+        if i == 0:
+            print_fibonacci.append(0)
+        elif i == 1:
+            print_fibonacci.append(1)
+        else:
+            next_number = print_fibonacci[i - 1] + print_fibonacci[i - 2]
+            print_fibonacci.append(next_number)
+
+    print(print_fibonacci[:length])
